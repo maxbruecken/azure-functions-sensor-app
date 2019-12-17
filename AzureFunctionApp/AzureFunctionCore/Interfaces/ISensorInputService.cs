4b37100448f49161e23a517144a423e875be3062
Maxim Becker <maxim.becker@andrena.de>
2019-12-17 16:22:31 +0100

    implemented first function

---
+++ AzureFunctionApp/AzureFunctionCore/Interfaces/ISensorInputService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using AzureFunctionCore.Models;
+
+namespace AzureFunctionCore.Interfaces
+{
+    public interface ISensorInputService
+    {
+        Task ProcessInputAsync(SensorInput input);
+    }
+}
---
+++ AzureFunctionApp/AzureFunctionCore/Services/SensorInputService.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using AzureFunctionCore.Interfaces;
+using AzureFunctionCore.Models;
+using Microsoft.Extensions.Logging;
+
+namespace AzureFunctionCore.Services
+{
+    public class SensorInputService : ISensorInputService
+    {
+        private readonly ILogger<ISensorInputService> _logger;
+
+        public SensorInputService(ILogger<ISensorInputService> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task ProcessInputAsync(SensorInput input)
+        {
+            _logger.LogInformation($"Process input {input.Data}");
+            return Task.CompletedTask;
+        }
+    }
+}
